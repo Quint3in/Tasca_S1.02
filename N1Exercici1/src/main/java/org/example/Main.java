7be9e250fcf0739ce5fb8b534d6a27ebb6a7cbc1
JoelQuintana <joelqn95@gmail.com>
2026-02-23 12:33:46 +0100

    feat(n1exercici1): implement sale testing in Main

---
+++ N1Exercici1/src/main/java/org/example/Main.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import org.example.exception.EmptySaleException;
+
+public class Main {
+    static void main() {
+        Product product1 = new Product("product1",2.5);
+        Product product2 = new Product("product2",10);
+
+        Sale sale1 = new Sale();
+
+        sale1.addProduct(product1);
+        sale1.addProduct(product2);
+
+        try {
+            System.out.println(sale1.calculateTotal());
+        } catch (EmptySaleException e) {
+            throw new RuntimeException(e);
+        }
+
+        Sale sale2 = new Sale();
+
+        try {
+            System.out.println(sale2.calculateTotal());
+        } catch (EmptySaleException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+    }
+}
