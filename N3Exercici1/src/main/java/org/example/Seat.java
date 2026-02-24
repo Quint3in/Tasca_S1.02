b96585d36ff008f06a29e976e23f758913530f83
JoelQuintana <joelqn95@gmail.com>
2026-02-24 10:37:21 +0100

    feat(n3exercici1): add Seat model

--- N3Exercici1/src/main/java/org/example/Seat.java
+++ N3Exercici1/src/main/java/org/example/Seat.java
@@ -1,4 +1,46 @@
 package org.example;
 
 public class Seat {
+    private int row;
+    private int seat;
+    private String personName;
+
+    public Seat(int row, int seat, String personName) {
+        this.row = row;
+        this.seat = seat;
+        this.personName = personName;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getSeat() {
+        return seat;
+    }
+
+    public void setSeat(int seat) {
+        this.seat = seat;
+    }
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+
+    @Override
+    public String toString() {
+        return "Seat{" +
+                "row=" + row +
+                ", seat=" + seat +
+                ", personName='" + personName + '\'' +
+                '}';
+    }
 }
